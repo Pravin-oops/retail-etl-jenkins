48c915b071a2332d35b9ec27a187f5f75ffe5061
Pravin <pravin.puducherry@gmail.com>
2026-01-01 17:08:52 +0530

    Oracle can take the file from Local build

--- sql/02_directory_creation.sql
+++ sql/02_directory_creation.sql
@@ -1,7 +1,7 @@
 -- Run this as RETAIL_DW
 -- Create a logical reference to the physical folder inside the Docker container
--- Note: /opt/oracle/oradata/input_data is the path we defined in docker-compose.yml
-CREATE OR REPLACE DIRECTORY source_data_dir AS '/opt/oracle/oradata/input_data';
+-- Note: /data is the path we defined in docker-compose.yml
+CREATE OR REPLACE DIRECTORY source_data_dir AS '/data';
 -- Verify access (This checks if Oracle can "see" the folder)
 SELECT * FROM all_directories WHERE directory_name = 'SOURCE_DATA_DIR';
 
